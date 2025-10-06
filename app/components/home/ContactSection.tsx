36fd258a936df092f3b232ea040a43b38913ea0b
williamjaysmith <williamjaysmith@gmail.com>
2025-10-05 23:40:43 -0500

    removed get in touch

--- app/components/home/ContactSection.tsx
+++ app/components/home/ContactSection.tsx
@@ -48,41 +48,6 @@ export default function ContactSection() {
           ))}
         </div>
 
-        <motion.div
-          initial={{ opacity: 0, y: 30 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          className="mt-20 text-center"
-        >
-          <motion.a
-            href="mailto:williamjaysmith@gmail.com"
-            animate={controls}
-            initial={{ boxShadow: "0px 0px 0px #2c2c2c" }}
-            whileHover={{
-              y: -6,
-              rotate: -2,
-              boxShadow: "6px 6px 0px #2c2c2c",
-              transition: { duration: 0.2 },
-            }}
-            whileTap={{
-              x: 3,
-              y: -3,
-              rotate: -2,
-              boxShadow: "3px 3px 0px #2c2c2c",
-              transition: { duration: 0.1 },
-            }}
-            className="inline-flex items-center gap-2 rounded-2xl border-3 border-[#2c2c2c] bg-[#2c2c2c] button-text code-view-button whitespace-nowrap text-white transition-colors hover:bg-white hover:text-[#2c2c2c]"
-            style={{
-              fontSize: 'var(--button-font-size)',
-              paddingLeft: 'var(--button-px)',
-              paddingRight: 'var(--button-px)',
-              paddingTop: 'var(--button-py)',
-              paddingBottom: 'var(--button-py)',
-            }}
-          >
-            GET IN TOUCH <ChevronRight className="w-6 h-6" strokeWidth={3} />
-          </motion.a>
-        </motion.div>
       </div>
     </div>
   );
