d98c59311af4f2e17e55f1eb11b7bfe525028f88
williamjaysmith <williamjaysmith@gmail.com>
2026-04-10 23:05:55 -0500

    trying to fix track drag feature

--- app/skyhammer/page.tsx
+++ app/skyhammer/page.tsx
@@ -146,7 +146,8 @@ export default function Skyhammer() {
   const sensors = useSensors(
     useSensor(PointerSensor, {
       activationConstraint: {
-        distance: 3,
+        delay: 250,
+        tolerance: 5,
       },
     }),
     useSensor(KeyboardSensor, {
